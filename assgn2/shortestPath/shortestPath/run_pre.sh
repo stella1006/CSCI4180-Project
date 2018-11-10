82048ae27387e162d55b3b7bc7a8f94d1efcb1dd
stella1006 <stella.lee.lzr@gmail.com>
2018-11-10 11:37:35 +0800

    PDPreProcess

---
+++ assgn2/shortestPath/shortestPath/run_pre.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+if [ "$#" -eq 1 ]; then
+    rm ./pre_output/*
+    hadoop fs -rm /lzr/input/*
+    hadoop fs -put test.txt /lzr/input/
+    hadoop fs -rm -r /lzr/output
+    hadoop com.sun.tools.javac.Main PDPreProcess.java
+    jar cf pdp.jar PDPreProcess*.class
+    hadoop jar pdp.jar PDPreProcess /lzr/input /lzr/output
+    hadoop fs -get /lzr/output/* pre_output/
+else
+rm ./pre_output/*
+hadoop fs -rm -r /lzr/output
+hadoop com.sun.tools.javac.Main PDPreProcess.java 
+jar cf pdp.jar PDPreProcess*.class
+hadoop jar pdp.jar PDPreProcess /lzr/input /lzr/output
+hadoop fs -get /lzr/output/* pre_output/
+fi
