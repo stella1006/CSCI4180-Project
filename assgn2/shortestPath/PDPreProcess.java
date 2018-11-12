d41bbf3df883c16caa53ef08b868404bd6d84ce4
stella1006 <stella.lee.lzr@gmail.com>
2018-11-12 10:08:16 +0800

    iterations

--- assgn2/shortestPath/PDPreProcess.java
+++ assgn2/shortestPath/PDPreProcess.java
@@ -6,8 +6,8 @@ import java.util.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,37 +15,46 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class PDPreProcess extends Mapper<Object, Text, NullWritable, PDNodeWritable> {
+public class PDPreProcess extends Mapper<Object, Text, PDNodeWritable, NullWritable> {
     //public static class ParsingMapper
 
 
         // public void setup(Context context) throws IOException, InterruptedException {
         //
         // }
-        Map< Integer, PDNodeWritable> hmap = new HashMap< Integer,PDNodeWritable>();
+        Map<Integer, PDNodeWritable> hmap = new HashMap<Integer,PDNodeWritable>();
+        Map<IntWritable, Integer> flag = new HashMap<IntWritable, Integer>();
+
         @Override
         public void map(Object key, Text value, Context context
                 ) throws IOException, InterruptedException {
             String[] list=value.toString().split(" ");
             Integer k = Integer.parseInt(list[0]);
-            Integer des = Integer.parseInt(list[1]);
-            Integer weights = Integer.parseInt(list[2]);
+            IntWritable kk = new IntWritable(k);
+            IntWritable des = new IntWritable(Integer.parseInt(list[1]));
+            IntWritable weights = new IntWritable(Integer.parseInt(list[2]));
             //String outstring = "(" + Integer.toString(des) + " " + Integer.toString(weights) + ")";
+            flag.put(kk, 1);
+            flag.put(des, 1);
 
             if (!hmap.containsKey(k)) {
-                PDNodeWritable node = new PDNodeWritable(k);
-                ArrayList<Integer> item = new ArrayList<Integer>();
+                PDNodeWritable node = new PDNodeWritable(kk);
+                ArrayList<IntWritable> item = new ArrayList<IntWritable>();
                 item.add(des);
                 item.add(weights);
                 node.addAdjList(item);
+                //hmap.put(k, new Text(outstring));
                 hmap.put(k, node);
             }
             else {
                 PDNodeWritable temp_node = hmap.get(k);
-                ArrayList<Integer> item = new ArrayList<Integer>();
+                ArrayList<IntWritable> item = new ArrayList<IntWritable>();
                 item.add(des);
                 item.add(weights);
                 temp_node.addAdjList(item);
+                // // String temp_node = hmap.get(k).toString();
+                // // temp_node += "," + outstring;
+                // //hmap.replace(k, new Text(temp_node));
                 hmap.replace(k, temp_node);
             }
             //Text te = new Text(outstring);
@@ -54,9 +63,18 @@ public class PDPreProcess extends Mapper<Object, Text, NullWritable, PDNodeWrita
 
         @Override
         public void cleanup(Context context) throws IOException, InterruptedException {
+            if (!flag.isEmpty()) {
+                for (Map.Entry<IntWritable, Integer> entry : flag.entrySet()) {
+                    if (!hmap.containsKey(entry.getKey().get())) {
+                        PDNodeWritable node = new PDNodeWritable(entry.getKey());
+                        hmap.put(entry.getKey().get(), node);
+                    }
+                }
+            }
             if (!hmap.isEmpty()) {
                 for (Map.Entry<Integer, PDNodeWritable> entry : hmap.entrySet()) {
-                    context.write(NullWritable.get() , entry.getValue());
+                    context.write(entry.getValue() , NullWritable.get());
+                    //context.write(new IntWritable(entry.getKey()) , NullWritable.get());
                 }
             }
         }
--- assgn2/shortestPath/ParallelDijkstra.java
+++ assgn2/shortestPath/ParallelDijkstra.java
@@ -11,21 +11,119 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.InputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat.*;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.RecordReader;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.io.LongWritable;
+
+
+class PDNodeecordReader extends RecordReader<IntWritable,PDNodeWritable>  {
+    LineRecordReader lineRecordReader;
+    IntWritable key;
+    PDNodeWritable value;
+
+    @Override
+    public void initialize(InputSplit inputSplit, TaskAttemptContext context) throws IOException, InterruptedException {
+        lineRecordReader = new LineRecordReader();
+        lineRecordReader.initialize(inputSplit, context);
+    }
+
+    // It’s the method that will be used to transform the line into key value
+    @Override
+    public boolean nextKeyValue() throws IOException, InterruptedException {
+        if (!lineRecordReader.nextKeyValue()) {
+            return false;
+        }
+        String line = new String(lineRecordReader.getCurrentValue().toString());
+        //String[] keyValue = line.split("\t");
+        //String[] keyFields = keyValue[0].split(" ");
+        //valueFields = keyValue[1].split(" ");
+        //IntWritable temp = new IntWritable(line);
+        value = new PDNodeWritable();
+        //key.setText(line);
+        value.setFromText(line);
+        key = value.getNodeId();
+        //IntWritable nodeId = temp.getNodeId();
+        //key = new PDNodeWritable(nodeId);
+        //value = new PDNodeWritable(IntWritable);
+        return true;
+    }
+
+    @Override
+        public IntWritable getCurrentKey() throws IOException, InterruptedException {
+            return key;
+        }
+
+        @Override
+        public PDNodeWritable getCurrentValue() throws IOException, InterruptedException {
+            return value;
+        }
+
+        @Override
+        public float getProgress() throws IOException, InterruptedException {
+            return 0;
+        }
+
+        @Override
+        public void close() throws IOException {
+            lineRecordReader.close();
+        }
+}
+
+class PDNodeInputFormat extends FileInputFormat<IntWritable,PDNodeWritable> {
+      @Override
+      public RecordReader<IntWritable,PDNodeWritable> createRecordReader(InputSplit inputSplit,TaskAttemptContext context)
+          throws IOException, InterruptedException {
+
+          PDNodeecordReader pdRecordReader = new PDNodeecordReader();
+          pdRecordReader.initialize(inputSplit, context);
+          return pdRecordReader;
+      }
+  }
+
 
 public class ParallelDijkstra {
-    public static class DijkstraMapper
-            extends Mapper<Object, Text, IntWritable, PDNodeWritable>{
-                public void setup(Context context) throws IOException, InterruptedException {
+    public static enum ReachCounter { COUNT };
 
+    public static class DijkstraMapper
+            extends Mapper<IntWritable, PDNodeWritable, IntWritable, PDNodeWritable>{
+
+                public void setup(Context context) throws IOException, InterruptedException {
                 }
 
-                public void map(Object key, PDNodeWritable value, Context context
+                public void map(IntWritable key, PDNodeWritable value, Context context
                         ) throws IOException, InterruptedException {
-                            context.write(new IntWritable(0), value);
+                            //context.write(value.getNodeId(), value);
+                            Configuration conf = context.getConfiguration();
+                            int src = Integer.parseInt(conf.get("src"));
+
+                            if (key.get() == src) {
+                                value.setDistance(new IntWritable(0));
+                            }
+                            IntWritable d = value.getDistance();
+                            context.write(key, value);
+
+                            //for (ArrayList<IntWritable> node : value.getAdjList()) {
+                            for (int i = 0; i < value.getAdjList().size(); i++) {
+                                ArrayList<IntWritable> node = value.getAdjList().get(i);
+                                IntWritable tmp_id = new IntWritable(0);
+                                IntWritable tmp_dis = new IntWritable(0);
+                                if (d.get() == Integer.MAX_VALUE) {
+                                    tmp_dis.set(Integer.MAX_VALUE);
+                                } else {
+                                    tmp_dis.set(d.get()+node.get(1).get());
+                                }
+                                PDNodeWritable pass = new PDNodeWritable(tmp_id, tmp_dis, key);
+                                context.write(node.get(0), pass);
+                            }
+
                         }
                 public void cleanup(Context context) throws IOException, InterruptedException {
 
@@ -38,19 +136,42 @@ public class ParallelDijkstra {
             public void reduce(IntWritable key, Iterable<PDNodeWritable> values,
                     Context context
                     ) throws IOException, InterruptedException {
-                        for (PDNodeWritable val : values) {
-                            context.write(NullWritable.get(), val);
+                    // for (PDNodeWritable val : values) {
+                    //     context.write(key, val);
+                    // }
+
+                    IntWritable d_min = new IntWritable((Integer.MAX_VALUE));
+                    PDNodeWritable M = new PDNodeWritable();
+                    for (PDNodeWritable node : values) {
+                        if (node.getNodeId().get() != 0) {
+                            M.setNode(node);
+                            //context.write(key, M);
+                            //d_min.set(M.getDistance().get());
+                        }
+                        else if (node.getDistance().get() < d_min.get()) {
+                            d_min.set(node.getDistance().get());
                         }
                     }
+                    if (d_min.get() < M.getDistance().get()) {
+                        context.getCounter(ReachCounter.COUNT).increment(1);
+                        M.setDistance(d_min);
+                    }
+                    context.write(NullWritable.get(), M);
+                }
     }
 
     public static void main(String[] args) throws Exception {
         //Configuration conf1 = new Configuration();
         //Configuration conf2 = new Configuration();
+        int iterations = Integer.parseInt(args[3]);
+
 
         JobControl jobControl = new JobControl("jobChain");
 
         Configuration conf = new Configuration();
+        conf.set("src", args[2]);
+        conf.set("iterations", args[3]);
+
         Job job1 = Job.getInstance(conf);
         Job job2 = Job.getInstance(conf);
 
@@ -61,27 +182,31 @@ public class ParallelDijkstra {
         //job1.setCombinerClass(ComReducer.class);
         job1.setNumReduceTasks(0);
         // job1.setReducerClass(ComReducer.class);
-        job1.setOutputKeyClass(NullWritable.class);
-        job1.setOutputValueClass(PDNodeWritable.class);
+        job1.setOutputKeyClass(PDNodeWritable.class);
+        job1.setOutputValueClass(NullWritable.class);
         FileInputFormat.setInputPaths(job1, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job1, new Path("/temp"));
+        FileOutputFormat.setOutputPath(job1, new Path("/lzr/temp/out_0"));
 
 
         job2.setJarByClass(ParallelDijkstra.class);
         job2.setJobName("ParallelDijkstra");
 
         job2.setMapperClass(DijkstraMapper.class);
-        job2.setCombinerClass(DijkstraReducer.class);
+        //job2.setCombinerClass(DijkstraReducer.class);
         job2.setReducerClass(DijkstraReducer.class);
+        job2.setMapOutputKeyClass(IntWritable.class);
+        job2.setMapOutputValueClass(PDNodeWritable.class);
         job2.setOutputKeyClass(NullWritable.class);
         job2.setOutputValueClass(PDNodeWritable.class);
-        FileInputFormat.addInputPath(job2, new Path("/temp"));
-        FileOutputFormat.setOutputPath(job2, new Path(args[1]));
+        job2.setInputFormatClass(PDNodeInputFormat.class);
+        FileInputFormat.addInputPath(job2, new Path("/lzr/temp/out_0"));
+        FileOutputFormat.setOutputPath(job2, new Path("/lzr/temp/out_1"));
 
         //System.exit(job.waitForCompletion(true) ? 0 : 1);
         ControlledJob controlledJob1 = new ControlledJob(conf);
         ControlledJob controlledJob2 = new ControlledJob(conf);
 
+        //job1.waitForCompletion(true);
         controlledJob1.setJob(job1);
         controlledJob2.setJob(job2);
         // make job2 dependent on job1
@@ -93,13 +218,69 @@ public class ParallelDijkstra {
         Thread jcThread = new Thread(jobControl);
         jcThread.start();
 
+        long temp_counts;
         //while(true){
-        while (!jobControl.allFinished()){
-            //System.out.println(jobControl.getSuccessfulJobList());
-            //System.exit(0);
+        while(true){
+            if(jobControl.allFinished()){
+                temp_counts = job2.getCounters().findCounter(ParallelDijkstra.ReachCounter.COUNT ).getValue();
+                System.out.println(temp_counts);
+                System.out.println(jobControl.getSuccessfulJobList());
+                jobControl.stop();
+                if(jobControl.getFailedJobList().size() > 0){
+                    System.out.println(jobControl.getFailedJobList());
+                }
+                break;
+            }
         }
-        System.out.println(jobControl.getSuccessfulJobList());
-        jobControl.stop();
+        // if (iterations != 0) {
+        if (iterations == 0) {
+            iterations = Integer.MAX_VALUE;
+        }
+        int flag = 0;
+        for (int i = 1; i < iterations; i++) {
+            String last_out = "/lzr/temp/out_" + Integer.toString(i);
+            String next_out = "";
+            if (i == iterations-1 || flag == 0) {
+                next_out = args[1];
+            } else {
+                next_out = "/lzr/temp/out_" + Integer.toString(i+1);
+            }
+
+            Job job_new = new Job(conf, "ParallelDijkstra_" + i);
+            job_new.setJarByClass(ParallelDijkstra.class);
+            job_new.setMapperClass(DijkstraMapper.class);
+            //job_new.setCombinerClass(DijkstraReducer.class);
+            job_new.setReducerClass(DijkstraReducer.class);
+            job_new.setMapOutputKeyClass(IntWritable.class);
+            job_new.setMapOutputValueClass(PDNodeWritable.class);
+            job_new.setOutputKeyClass(NullWritable.class);
+            job_new.setOutputValueClass(PDNodeWritable.class);
+            job_new.setInputFormatClass(PDNodeInputFormat.class);
+
+            FileInputFormat.addInputPath(job_new, new Path(last_out));
+            FileOutputFormat.setOutputPath(job_new, new Path(next_out));
+            job_new.waitForCompletion(true);
+
+            temp_counts = job_new.getCounters().findCounter(ParallelDijkstra.ReachCounter.COUNT ).getValue();
+            System.out.println("Finish_" + i + "_" + temp_counts);
+            if (temp_counts == 0) {
+                flag = 1;
+            }
+        }
+        // }
+        // } else {
+        //     int i=1;
+        //     while (true) {
+        //         String last_out = "/lzr/temp/out_" + Integer.toString(i);
+        //         String next_out = "/lzr/temp/out_" + Integer.toString(i+1);
+        //         FileInputFormat.addInputPath(job2, new Path(last_out));
+        //         FileOutputFormat.setOutputPath(job2, new Path(next_out));
+        //         job2.waitForCompletion(true);
+        //         temp_countsjob2.getCounters().findCounter(ParallelDijkstra.ReachCounter.COUNT ).getValue();
+        //         if (temp_counts == 0) break;
+        //         i++;
+        //     }
+        // }
         //}
     }
 }
